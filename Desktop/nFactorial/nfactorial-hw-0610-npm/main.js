161971c4b1a447c37b3bab2a08fb7d77b7e0c222
alibinauanov <an3502@nyu.edu>
2022-06-13 19:11:42 +0600

    gold done

---
+++ Desktop/nFactorial/nfactorial-hw-0610-npm/main.js
@@ -0,0 +1,24 @@
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+
+function toggleTheme() {
+   if (localStorage.getItem('theme') === 'theme-dark'){
+       setTheme('theme-light');
+   } else {
+       setTheme('theme-dark');
+   }
+}
+
+(function () {
+   if (localStorage.getItem('theme') === 'theme-dark') {
+       setTheme('theme-dark');
+   } else {
+       setTheme('theme-light');
+   }
+});
+
+const setCookie = () => {
+    document.cookie = "user=John Doe; expires=Wed, 15 June 2022 22:00:00 GMT"
+}
